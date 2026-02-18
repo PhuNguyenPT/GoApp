9986919caf01a2355e32528591b260d00d0f3c22
phunpt <phu.npt01@gmail.com>
2026-02-18 14:45:29 +0700

    add GIN_MODE

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -1,20 +1,25 @@
 package server
 
 import (
-	"net/http"
 	"fmt"
 	"log"
+	"net/http"
+	"os"
 	"time"
+
+	"github.com/coder/websocket"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"github.com/coder/websocket"
-	
+
 	"GoApp/internal/views"
 )
 
 func (s *Server) RegisterRoutes() http.Handler {
-	r := gin.Default()
-
+	gin.SetMode(os.Getenv("GIN_MODE"))
+	r := gin.New()
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+	
 	apiGroup := r.Group("/api")
 	apiGroup.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"http://localhost:5173"},
