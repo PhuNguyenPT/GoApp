9d309b3776f6d7ddecc51ca6261cfc47c8c08a43
phunpt <phu.npt01@gmail.com>
2026-02-23 20:19:27 +0700

    update middlewares to fix warning on crawler param

--- crawler/middlewares.py
+++ crawler/middlewares.py
@@ -2,12 +2,13 @@ from fake_useragent import UserAgent
 
 
 class RandomUserAgentMiddleware:
-    def __init__(self):
+    def __init__(self, crawler):
         self.ua = UserAgent()
+        self.crawler = crawler
 
     @classmethod
     def from_crawler(cls, crawler):
-        return cls()
+        return cls(crawler)
 
-    def process_request(self, request, spider=None):
+    def process_request(self, request):
         request.headers["User-Agent"] = self.ua.random
